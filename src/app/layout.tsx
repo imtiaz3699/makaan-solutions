5d16b193fb39796be7807fb01f1420d5620a0648
imtiaz3699 <imtiazali78652@gmail.com>
2023-09-04 02:50:21 -0700

    last

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className='bg-black grid place-items-center'>
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className='grid place-items-center'>
+      <body className={inter.className} style={{backgroundColor:'black'}}>{children}</body>
     </html>
   )
 }
